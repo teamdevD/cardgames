27972474d7c515dda90e4c6a4b59db4e168d23da
Taiga2022 <f2021212010@gmail.com>
2024-01-13 11:35:22 +0000

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: process.env.GITHUB_PAGES  // この行を追加
-        ? "cardgames"            // この行を追加
-        : "./",                     // この行を追加
-    plugins: [react()],
-});
+  plugins: [react()],
+})
